77f0d242b02cfa249baefd9c3d5ab95a723124a4
samuel_wiese <samuel.wiese@rsu.de>
2021-12-08 14:08:45 +0100

    add category dropdown menu with functionality

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('posts', [
-        'posts' => Post::latest('published_at')->get()
+        'posts' => Post::latest('published_at')->get(),
+        'categories' => Category::all()
     ]);
 });
 
@@ -31,12 +32,15 @@ Route::get('/posts/{post}', function (Post $post) {
 
 Route::get('/categories/{category:slug}', function (Category $category) {
     return view('posts', [
-        'posts' => $category->posts
+        'posts' => $category->posts,
+        'currentCategory' => $category,
+        'categories' => Category::all()
     ]);
 });
 
 Route::get('/authors/{author:username}', function (User $author) {
     return view('posts', [
-        'posts' => $author->posts
+        'posts' => $author->posts,
+        'categories' => Category::all()
     ]);
 });
