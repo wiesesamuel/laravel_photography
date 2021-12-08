b53abc6b32c21869bdf7fe152c179142ea3b2051
samuel_wiese <samuel.wiese@rsu.de>
2021-12-08 09:41:24 +0100

    add db factorys

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Category;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+class PostFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'category_id' => Category::factory(),
+            'user_id' => User::factory(),
+            'slug' => $this->faker->slug,
+            'title' => $this->faker->sentence,
+            'body' => $this->faker->paragraph,
+            'excerpt' => $this->faker->sentence,
+        ];
+    }
+
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,64 +16,15 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        // drop tables
         User::truncate();
         Post::truncate();
         Category::truncate();
 
-        $user = User::factory()->create();
+        // generate tables
+        $users = User::factory(20)->create();
+        $posts = Post::factory(20)->create();
+        $categorys = Category::factory(4)->create();
 
-        $private = Category::create([
-            'name' => 'Persönlich',
-            'slug' => 'private',
-            'description' => 'Persönliches'
-        ]);
-
-        $family = Category::create([
-            'name' => 'Familie',
-            'slug' => 'family',
-            'description' => 'Klein und Groß'
-        ]);
-
-        $art = Category::create([
-            'name' => 'Kunst',
-            'slug' => 'art',
-            'description' => 'Natur und Kunst'
-        ]);
-
-        Post::create([
-            'user_id' => $user->id,
-            'category_id' => $family->id,
-            'title' => 'My family post',
-            'slug' => 'famaily-post-u-should-see',
-            'excerpt' => 'Familie ist alles',
-            'body' => 'oiqretjoiqwnuecfoiweqhnfckjqshndfjaugs',
-        ]);
-
-        Post::create([
-            'user_id' => $user->id,
-            'category_id' => $art->id,
-            'title' => 'My art post',
-            'slug' => 'art-post',
-            'excerpt' => 'Kunstvorschau',
-            'body' => 'asdfasdfasdfasdfasdfasdfasdf',
-        ]);
-
-        Post::create([
-            'user_id' => $user->id,
-            'category_id' => $art->id,
-            'title' => 'My second art post',
-            'slug' => 'art-post-nr2',
-            'excerpt' => 'Kunstvorschau 2',
-            'body' => 'akisdhgoiabsdgkjabskdjfbgaisouhfdiouawbeikurbfksajdfb',
-        ]);
-
-        Post::create([
-            'user_id' => $user->id,
-            'category_id' => $private->id,
-            'title' => 'My private post',
-            'slug' => 'dont-watch-me',
-            'excerpt' => 'vorschau zum abschauen',
-            'body' => 'oijaujg098q43t9ohis',
-        ]);
     }
 }
