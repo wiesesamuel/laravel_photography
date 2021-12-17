04ec33b8dea0b17bda62450e15dd58e332a86a99
Your Name <you@example.com>
2021-12-17 13:57:48 +0100

    add global navigationbar

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -7,22 +7,8 @@
 <script defer src="https://unpkg.com/alpinejs@3.7.0/dist/cdn.min.js"></script>
 
 <body style="font-family: Open Sans, sans-serif">
+@include('layouts.navigation-home')
 <section class="px-6 py-8">
-    <nav class="md:flex md:justify-between md:items-center">
-        <div>
-            <a href="/">
-                <img src="/images/logo.svg" alt="Laracasts Logo" width="165" height="16">
-            </a>
-        </div>
-
-        <div class="mt-8 md:mt-0">
-            <a href="/" class="text-xs font-bold uppercase">Home Page</a>
-
-            <a href="#" class="bg-blue-500 ml-3 rounded-full text-xs font-semibold text-white uppercase py-3 px-5">
-                Subscribe for Updates
-            </a>
-        </div>
-    </nav>
 
     {{$slot}}
 
