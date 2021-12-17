43aa0707aa5fba2a42a1de8ed587cf7fe7281d4d
Your Name <you@example.com>
2021-12-17 13:11:19 +0100

    breeze:install user registration etc. Move Views to Subfolders

---
+++ resources/views/auth/confirm-password.blade.php
@@ -0,0 +1,36 @@
+<x-guest-layout>
+    <x-auth.auth-card>
+        <x-slot name="logo">
+            <a href="/">
+                <x-auth.application-logo class="w-20 h-20 fill-current text-gray-500" />
+            </a>
+        </x-slot>
+
+        <div class="mb-4 text-sm text-gray-600">
+            {{ __('This is a secure area of the application. Please confirm your password before continuing.') }}
+        </div>
+
+        <!-- Validation Errors -->
+        <x-auth.auth-validation-errors class="mb-4" :errors="$errors" />
+
+        <form method="POST" action="{{ route('password.confirm') }}">
+            @csrf
+
+            <!-- Password -->
+            <div>
+                <x-auth.label for="password" :value="__('Password')" />
+
+                <x-auth.input id="password" class="block mt-1 w-full"
+                                type="password"
+                                name="password"
+                                required autocomplete="current-password" />
+            </div>
+
+            <div class="flex justify-end mt-4">
+                <x-auth.button>
+                    {{ __('Confirm') }}
+                </x-auth.button>
+            </div>
+        </form>
+    </x-auth.auth-card>
+</x-guest-layout>
---
+++ resources/views/auth/reset-password.blade.php
@@ -0,0 +1,48 @@
+<x-guest-layout>
+    <x-auth.auth-card>
+        <x-slot name="logo">
+            <a href="/">
+                <x-auth.application-logo class="w-20 h-20 fill-current text-gray-500" />
+            </a>
+        </x-slot>
+
+        <!-- Validation Errors -->
+        <x-auth.auth-validation-errors class="mb-4" :errors="$errors" />
+
+        <form method="POST" action="{{ route('password.update') }}">
+            @csrf
+
+            <!-- Password Reset Token -->
+            <input type="hidden" name="token" value="{{ $request->route('token') }}">
+
+            <!-- Email Address -->
+            <div>
+                <x-auth.label for="email" :value="__('Email')" />
+
+                <x-auth.input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email', $request->email)" required autofocus />
+            </div>
+
+            <!-- Password -->
+            <div class="mt-4">
+                <x-auth.label for="password" :value="__('Password')" />
+
+                <x-auth.input id="password" class="block mt-1 w-full" type="password" name="password" required />
+            </div>
+
+            <!-- Confirm Password -->
+            <div class="mt-4">
+                <x-auth.label for="password_confirmation" :value="__('Confirm Password')" />
+
+                <x-auth.input id="password_confirmation" class="block mt-1 w-full"
+                                    type="password"
+                                    name="password_confirmation" required />
+            </div>
+
+            <div class="flex items-center justify-end mt-4">
+                <x-auth.button>
+                    {{ __('Reset Password') }}
+                </x-auth.button>
+            </div>
+        </form>
+    </x-auth.auth-card>
+</x-guest-layout>
