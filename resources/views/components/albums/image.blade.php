2c0aadc00e8c543fbf3b66e703debcf7cf39ca3d
Your Name <you@example.com>
2021-12-18 16:44:51 +0100

    rm footer

--- resources/views/components/albums/image.blade.php
+++ resources/views/components/albums/image.blade.php
@@ -2,7 +2,7 @@
 {{--@dd($image)--}}
 {{--<img src="/images/illustration-1.png" alt="Blog image illustration" class="rounded-xl">--}}
     <img
-        class="object-contain"
+        class="object-contain rounded-xl"
         src="{{$image->url}}"
         title="{{$image->title}}"
         alt="{{isset($image->description) ?? ''}}"
