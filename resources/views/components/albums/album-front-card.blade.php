bcd0d9670573f6d8b52ef8f15bfe204d3d3f1cbd
Your Name <you@example.com>
2021-12-18 21:15:54 +0100

    add todo for grid layout

--- resources/views/components/albums/album-front-card.blade.php
+++ resources/views/components/albums/album-front-card.blade.php
@@ -1,8 +1,16 @@
 @props(['album'])
-<div class="my-1 px-1 w-1/2 overflow-hidden sm:my-1 sm:px-1 sm:w-1/2 md:my-1 md:px-1 md:w-1/2 lg:my-1 lg:px-1 lg:w-1/3 xl:my-1 xl:px-1 xl:w-1/3
-@if (isset($album->coverImage->horizontal) && ($album->coverImage->horizontal == 1)) col-span:2 @endif">
+<div class="my-1 px-1 w-1/2 overflow-hidden
+{{--ToDo fix grid layout--}}
+@if (isset($album->coverImage->horizontal) && ($album->coverImage->horizontal == 1))
+    object-scale-down
+@else
+            object-contain hover:object-scale-down
+@endif
+            sm:my-1 sm:px-1 sm:w-1/2
+            md:my-1 md:px-1 md:w-1/2
+            lg:my-1 lg:px-1 lg:w-1/3
+            xl:my-1 xl:px-1 xl:w-1/3 ">
     <img
-        class="object-contain hover:object-scale-down"
         src="{{$album->coverImage->url}}"
         title="{{$album->name}}"
         alt="{{$album->description}}"
--- resources/views/components/albums/albums-grid.blade.php
+++ resources/views/components/albums/albums-grid.blade.php
@@ -1,5 +1,9 @@
 @if($albums->count() > 0)
-<div class="flex flex-wrap -mx-1 overflow-hidden sm:-mx-1 md:-mx-1 lg:-mx-1 xl:-mx-1">
+<div class="flex flex-wrap -mx-1 overflow-hidden
+sm:-mx-1
+md:-mx-1
+lg:-mx-1
+xl:-mx-1">
 
     @foreach($albums as $album)
         <x-albums.album-front-card
