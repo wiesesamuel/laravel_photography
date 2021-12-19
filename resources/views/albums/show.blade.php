fc345a6bcdc6c1a510d5d5b2605d240dc44a77ad
Your Name <you@example.com>
2021-12-19 18:33:10 +0100

    change show album to same grid to albums-grid

--- resources/views/albums/show.blade.php
+++ resources/views/albums/show.blade.php
@@ -3,8 +3,17 @@
         <x-albums.image-slider
             :images="$album->images"
         />
-        <x-albums.image-grid-with-featured
-            :images="$album->images"
-        />
+        <x-albums.grid.dense-grid-layout>
+            @foreach($album->images as $image)
+                <x-albums.grid.dense-grid-element
+                    :horizontal="$image->horizontal"
+                >
+                    <x-albums.image
+                        :image="$image"
+                        onclick="document.location='/images/{{$image->id}}'; return false;"
+                    />
+                </x-albums.grid.dense-grid-element>
+            @endforeach
+        </x-albums.grid.dense-grid-layout>
     </main>
 </x-layout>
