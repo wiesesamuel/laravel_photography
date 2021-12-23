38df9554306a8300f7340461ca66a8882fc25a47
Samuel Wiese <samuel.wiese@uni-ulm.de>
2021-12-23 22:48:15 +0100

    add faker in production

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -5,7 +5,6 @@ namespace Database\Factories;
 use App\Models\Category;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
-use Illuminate\Support\Str;
 
 class PostFactory extends Factory
 {
--- database/factories/TagFactory.php
+++ database/factories/TagFactory.php
@@ -2,10 +2,7 @@
 
 namespace Database\Factories;
 
-use App\Models\Category;
-use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
-use Illuminate\Support\Str;
 
 class TagFactory extends Factory
 {
