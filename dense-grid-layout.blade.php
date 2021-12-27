2754ab02fa69fe488d8f5f52827962c2475be74b
Samuel Wiese <samuel.wiese@uni-ulm.de>
2021-12-27 19:13:04 +0100

    fix git and env config

--- dense-grid-layout.blade.php
+++
@@ -1,7 +0,0 @@
-<div class="min-h-screen flex items-center  bg-purple-500">
-    <div class="flex-1 mx-auto p-10">
-        <div class="grid grid-cols-6 gap-2 grid-flow-row-dense">
-            {{$slot}}
-        </div>
-    </div>
-</div>
