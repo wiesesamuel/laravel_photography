df3f15e9d58053d8ad85e0add415c8aa9a4914c0
samuel_wiese <samuel.wiese@rsu.de>
2021-12-27 15:38:58 +0100

    fix frontend responsive

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -1,6 +1,7 @@
 <!doctype html>
 
 <title>Wiese</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
 <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
 <link rel="preconnect" href="https://fonts.gstatic.com">
 <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700&display=swap" rel="stylesheet">
