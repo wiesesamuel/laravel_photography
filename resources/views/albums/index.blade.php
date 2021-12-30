bf491bdf97fa40acd43b2bc7fd69d387177178ff
Samuel Wiese <samuel.wiese@uni-ulm.de>
2021-12-30 02:03:31 +0100

    hotfix

--- resources/views/albums/index.blade.php
+++ resources/views/albums/index.blade.php
@@ -7,19 +7,21 @@
             @if (request('page') > 1)
                 {{$albums->links()}}
             @endif
-                    <x-albums.grid.dense-grid-layout>
+            <x-albums.grid.dense-grid-layout>
                 @foreach($albums as $album)
-                    <x-albums.grid.dense-grid-element
-                        :horizontal="$album->coverImage->orientation"
-                    >
-                        <x-albums.image
-                            :image="$album->coverImage"
-                            onclick="document.location='{{route('album', $album)}}'; return false;"
-                        />
-                    </x-albums.grid.dense-grid-element>
+                    @if ($album != null)
+                        <x-albums.grid.dense-grid-element
+                            :horizontal="$album->coverImage->orientation"
+                        >
+                            <x-albums.image
+                                :image="$album->coverImage"
+                                onclick="document.location='{{route('album', $album)}}'; return false;"
+                            />
+                        </x-albums.grid.dense-grid-element>
+                    @endif
                 @endforeach
             </x-albums.grid.dense-grid-layout>
-{{--            <x-albums.albums-grid :albums="$albums"></x-albums.albums-grid>--}}
+            {{--            <x-albums.albums-grid :albums="$albums"></x-albums.albums-grid>--}}
             {{$albums->links()}}
         @else
             <p class="text-center">No albums yet. Please come back later.</p>
